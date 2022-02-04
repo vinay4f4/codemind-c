612a7c71ab2d29d142a2af06420bfe51f10860ec
vinay G <98961157+vinay4f4@users.noreply.github.com>
2022-02-04 07:00:01 -0800

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d", &n);     for(i=1 ; i<=n ; i++)
        {
            for(j=1 ; j<=n ; j++)
            {
                printf("%c ",i+64);
            }
            printf("
    ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -2,12 +2,13 @@
 int main()
 {
     int n,i,j;
-    scanf("%d",&n);
+    scanf("%d", &n);
     for(i=1 ; i<=n ; i++)
+    
     {
-        for(j=1 ; j<=i ;j++)
+        for(j=1 ; j<=n ; j++)
         {
-            printf("%d",j);
+            printf("%c ",i+64);
         }
         printf("
 ");
